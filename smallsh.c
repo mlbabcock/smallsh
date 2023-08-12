6984a7fd0ce8a5ad897c3ffd9b7fb70bf407bb7e
mlbabcock <91307276+mlbabcock@users.noreply.github.com>
2023-08-11 23:56:48 -0700

    Update smallsh.c reworked code

--- smallsh.c
+++ smallsh.c
@@ -7,70 +7,66 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <string.h>
-#include <stdbool.h> 
+#include <fcntl.h> 
 #include <sys/wait.h> 
 #include <sys/stat.h> 
 #include <sys/types.h>
-#include <signal.h>
-#include <ctype.h>
 #include <stdint.h>
-
 
 #ifndef MAX_WORDS
 #define MAX_WORDS 512
 #endif
 
+#define MAX_LINE_LENGTH 1024
+#define MAX_WORD_COUNT 512
+
+// Global Functions
 char *words[MAX_WORDS];
 size_t wordsplit(char const *line);
 char * expand(char const *word);
-char pid_str[16];
 
-pid_t last_bg_pid = -1;                                 // Initialize with an invalid value
-pid_t get_last_bg_pid() {
-    return last_bg_pid;
+void manage_background_processes();
+void print_prompt();
+char** parse_line(char* line);
+int execute_command(char** words);
+void handle_signal(int signal);
+
+
+size_t wordsplit(char const *line);
+char param_scan(char const *word, char **start, char **end);
+char *build_str(char const *start, char const *end);
+char *expand(char const *word);
+
+// Global Variables
+char *line;
+char** words;
+int word_count;
+int last_exit_status;
+pid_t last_background_pid;
+
+int is_background_operator(char* word){
+    return strcmp(word, "&") == 0;
 }
 
-int last_exit_status = 0;                               // Initialize with a default value
-int get_last_exit_status() {
-    return last_exit_status;
-}
-
-void set_last_exit_status(int status) {
-    last_exit_status = status;
-}
-
-void sigint_handler(int sig) {
-    // do nothing 
-}
-
-void check_background_processes() {
-    pid_t pid;
-    int status;
-
-    // Wait for any background process without blocking
-    while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
-        if (WIFEXITED(status)) {
-            fprintf(stderr, "Child process %jd done. Exit status %d.\n", (intmax_t)pid, WEXITSTATUS(status));
-        } else if (WIFSIGNALED(status)) {
-            fprintf(stderr, "Child process %jd done. Signaled %d.\n", (intmax_t)pid, WTERMSIG(status));
-        }
-    }
+int is_redirection_operator(char* word){
+    return strcmp(word, ">") == 0 || strcmp(word, "<") == 0 || strcmp(word, ">>") == 0;
 }
 
 int main(int argc, char *argv[])
 {
-    // Signal handling for SIGINT
-    struct sigaction sa;
-    sa.sa_handler = sigint_handler;
-    sa.sa_flags = 0;
-    sigemptyset(&sa.sa_mask);
-    if (sigaction(SIGINT, &sa, NULL) == -1) {
-        perror("sigaction");
-        exit(EXIT_FAILURE);
-    }
+    // Signal Handlers
+    signal(SIGINT, handle_signal);
+    signal(SIGTSTP, handle_signal);
+
+    // Initialize last exit status and background PID
+    last_exit_status = 0;
+    last_background_pid = -1;
+
+    line = malloc(sizeof(char) * MAX_LINE_LENGTH);                      // Buffer to store user's input
 
     FILE *input = stdin;
     char *input_fn = "(stdin)";
+
     if (argc == 2) {
         input_fn = argv[1];
         input = fopen(input_fn, "re");
@@ -78,401 +74,172 @@ int main(int argc, char *argv[])
     } else if (argc > 2) {
         errx(1, "too many arguments");
     }
-    
-    char *line = NULL;
-    size_t n = 0;
-    int exit_status = 0;
-    
-    for (;;) {
+
+    while (1) {
     //prompt:;
-    /* TODO: Manage background processes */
-    check_background_processes();
+        /* TODO: Manage background processes */
+        manage_background_processes();
 
-    /* TODO: prompt */
-    if (input == stdin) {
-        char *ps1 = getenv("PS1");
-        if (ps1){
-            fprintf(stderr, "%s\n", expand(ps1));
-        }
-    }
+        /* TODO: prompt */
+        print_prompt();
 
-    continue_loop:
-        continue;
-    
-    end_of_input:
-        exit_status = get_status();
-        break;
+        // Read line of input
+        int length = MAX_LINE_LENGTH;
+        int line_len = getline(&line, &length, stdin);
 
-    prompt:
-        prompt();
-
-    // Read line of input
-    ssize_t line_len; 
-    do {
-        line_len = getline(&line, &n, input);
-        if (line_len < 0){
-            if (feof(input)) {
-                exit_status = get_status();                     // end of input
-                goto end_of_input;
-            } else {
-                fprintf(stderr, "\n");
-                prompt();
-                goto prompt;
-            }
-        }
-    } while (line_len < 0 && errno == EINTR);
-
-    // Handle errors
-    if (line_len < 0){
-        err(1, "%s", input_fn);
-    }
-
-    if (ferror(input)) {
-        clearerr(input);
-        errno = 0;
-    }
-
-    if (line[0] == '\0' || isspace(line[0])) {
-        goto continue_loop;                                               // If empty or only whitespace, continue loop
-    }
-    
-    size_t nwords = wordsplit(line);
-
-/* 
-   Parsing Part: Check for background operator and redirection
-    operators 
-*/
-    // Check for background operator
-    bool run_in_background = false;
-    if (nwords > 0 && strcmp(words[nwords - 1], "&") == 0) {
-        run_in_background = true;
-        free(words[nwords - 1]);
-        nwords--;
-    }
-
-    // Process redirection operators 
-    bool append_output = false;
-    for (size_t i = 0; i < nwords; ++i) {
-        if (strcmp(words[i], ">") == 0) {
-            if (i + 1 < words) {
-                freopen(words[i + 1], "w", stdout);
-                free(words[i]);
-                words[i] = NULL;
-                i++;
-            }
-        } else if (strcmp(words[i], ">>") == 0) {
-            if (i + 1 < nwords) {
-                append_output = true;
-                freopen(words[i + 1], "a", stdout);
-                free(words[i]);
-                words[i] = NULL;
-                i++;
-            }
-        } else if (strcmp(words[i], "<") == 0) {
-            if (i + 1 < nwords) {
-                freopen(words[i + 1], "r", stdin);
-                free(words[i]);
-                words[i] = NULL;
-                i++;
-            }
-        }
-    }
-
-    // Clear redirections, and reset stdout 
-    if (append_output){
-        freopen("/dev/tty", "a", stdout);
-    }
-
-/*
-    Part 5: Execution -- Built-in Commands
-    Exit and CD
-*/
-    if (nwords > 0) {
-        if (strcmp(words[0], "exit") == 0) {
-            if (nwords == 1) {                                      // Get exit status from last foreground command
-                int exit_status = get_last_exit_status();
-                goto continue_loop;
-            } else if (nwords == 2) {                               // Convert to integer
-                int exit_status = atoi(words[1]);
-                goto continue_loop;
-            } else {
-                fprintf(stderr, "exit: too many arguments\n");
-                goto prompt;
-            }
-        } else if (strcmp(words[0], "cd") == 0) {
-            if (nwords == 1) {
-                char *home = getenv("HOME");
-                if (home) {
-                    if (chdir(home) != 0) {
-                        perror("cd");
-                    }
-                } else {
-                    fprintf(stderr, "cd: HOME environment variable not set\n");
-                }
-            } else if (nwords == 2) {
-                if (chdir(words[1]) != 0) {
-                    perror("cd");
-                }
-            } else {
-                fprintf(stderr, "cd: too many arguments\n");
-            }
+        if (line_len == -1 && errno == EINTR) {
+            errno = 0;
+            printf("\n");
+            print_prompt();
             continue;
         }
+
+        char* words;
+        words = parse_line(line);
+        expand_words(words);
+        parse_words(words);
+        execute_command(words);
+
+        for (int i = 0; i < word_count; i++) {
+            free(words[i]);
+        }
+        free(words);
+    }
+    return 0;
+}
+
+void print_prompt(){
+    char* user = getenv("USER");
+    char* ps1 = getenv("PS1");
+    fprintf("%s%s$ ", user, ps1);
+}
+
+char** parse_line(char* line) {
+    char* word = strtok(line, " ");
+    char** words = malloc(sizeof(char*) * MAX_WORD_COUNT);
+
+    // iterate over words
+    int word_count = 0;
+    while (word != NULL) {
+        word = expand_word(word);
+
+        words[word_count] = word;
+        word = strtok(NULL, " ");
+        word_count++;
+    }
+    return words;
+}
+
+void handle_signal(int signal) {
+    if (signal == SIGINT) {
+        return;
     }
 
-/*
-    Part 5: Execution -- Non-Built-in Commands
-    Part 6: Waiting
-*/
-    pid_t child_pid = fork();
-    if (child_pid < 0) {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    } else if (child_pid == 0) {
-        // child processes 
+    if (signal == SIGTSTP) {
+        kill(getpid(), SIGSTOP);
+    }
+    errno = 0;
+    printf("\n");
+}
 
-        // Restore original signal dispositions
-        struct sigaction oldact;
-        if (sigaction(SIGINT, NULL, &oldact) == -1) {
-            perror("sigaction");
-            exit(EXIT_FAILURE);
-        }
-
-        if (oldact.sa_handler != SIG_DFL) {
-            if (sigaction(SIGINT, &oldact, NULL) == -1) {
-                perror("sigaction");
-                exit(EXIT_FAILURE);
+void parse_words(char** words){
+    for (int i = 0; i < word_count; i++) {
+        if (is_background_operator(words[i])) {
+            words[i] = NULL;
+            words[word_count++] = words[i + 1];
+            i++;
+        } else if (is_redirection_operator(words[i])) {
+            if (words[i + 1] != NULL) {
+                words[i] = words[i] + 1;
+                words[word_count++] = words[i + 1];
+                i++;
             }
         }
+    }
+}
 
-        for (size_t i = 0; i < nwords; ++i) {
-            if (strcmp(words[i], "<") == 0) {
-                if (i + 1 < nwords) {
-                    freopen(words[i + 1], "r", stdin);
-                    free(words[i]);
-                    free(words[i + 1]);
-                    words[i] = NULL;
-                    words[i + 1] = NULL;
-                    i++;
-                } 
-            } else if (strcmp(words[i], ">") == 0) {
-                if (i + 1 < nwords) {
-                    freopen(words[i + 1], "w", stdout); // Redirect stdout to the specified file (create/truncate)
-                    free(words[i]);
-                    free(words[i + 1]);
-                    words[i] = NULL;
-                    words[i + 1] = NULL;
-                    i++;
-                }
-            } else if (strcmp(words[i], ">>") == 0) {
-                if (i + 1 < nwords) {
-                    freopen(words[i + 1], "a", stdout); // Redirect stdout to the specified file (append)
-                    free(words[i]);
-                    free(words[i + 1]);
-                    words[i] = NULL;
-                    words[i + 1] = NULL;
-                    i++;
-                }
-            }
-        }
-
-        // execute non built-in commands
-        execvp(words[0], words);
-        
-        // Exec failed - error message 
-        perror("execvp");
-        exit(EXIT_FAILURE);
-
-    } else {
-        if(!run_in_background) {
-            int status;
-            pid_t waited_pid = waitpid(child_pid, &status, 0);
-            if (WIFEXITED(status)) {
-                set_last_exit_status(WEXITSTATUS(status));
-            } else if (WIFSIGNALED(status)) {
-                set_last_exit_status(128 + WTERMSIG(status));
-            }
+int execute_command(char** words) {
+    if (strcmp(words[0], "exit") == 0) {
+        int status;
+        if (words[1] != NULL) {
+            status = atoi(words[1]);
         } else {
-            fprintf("Background process started with PID %d\n", child_pid);
-            set_last_bg_pid(child_pid);
+            status = last_exit_status;
+        }
+        exit(status);
+    } else if (strcmp(words[0], "cd") == 0) {
+        if (words[1] == NULL) {
+            chdir("/");
+        } else {
+            chdir(words[1]);
+        }
+    } else {
+        int pid, status;
+        pid_t pid = fork();
+        if (pid == -1) {
+            perror("fork");
+            return -1;
+        } else if (pid == 0) {
+            // Ignore SIGINT and SIGTSTP
+            if (isatty(STDIN_FILENO)) {
+                signal(SIGINT, SIG_IGN);
+                signal(SIGTSTP, SIG_IGN);           
+            }
+
+            // Handle Redirection
+            for (int i = 0; i < word_count; i++) {
+                if (is_redirection_operator(words[i])) {
+                    char* filename = words[i + 1];
+                    int fd;
+                    if (strcmp(words[i], "<") == 0) {
+                        fd = open(filename, O_RDONLY);
+                    } else if (strcmp(words[i], ">") == 0) {
+                        fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0777);
+                    } else {
+                        fd = open(filename, O_WRONLY | O_APPEND | O_CREAT, 0777);
+                    }
+                    if (fd == -1) {
+                        perror("open");
+                        exit(1);
+                    }
+                    close(0);
+                    dup2(fd, 0);
+                    close(fd);
+                }
+            }
+            int status = execvp(words[0], words);
+            if (status == -1) {
+                perror("execvp");
+                exit(1);
+            }
+        } else {                                                                // Parent Process
+            if (!is_background_operator(words[word_count - 1])) {
+                waitpid(pid, &status, 0);
+                last_exit_status = status;
+            } else {
+                last_background_pid = pid;
+            }
         }
     }
-
-
-// OTHER PARTB OF CODE
-    for (size_t i = 0; i < nwords; ++i) {
-      fprintf(stderr, "Word %zu: %s  -->  ", i, words[i]);
-      char *exp_word = expand(words[i]);
-      free(words[i]);
-      words[i] = exp_word;
-      fprintf(stderr, "%s\n", words[i]);
-    }
-
-    struct command *cmd = parse_command(words, nwords);         // parse
-
-    exit_status = execute_command(cmd);
-
-    // Free memory 
-    for (size_t i = 0; i < nwords; ++i){
-        free(words[i]);
-    }
-    free(cmd);
-  }
-
-    free(line);  // Free memory allocated by getline
-
-    if(exit_status){
-        fprintf(stderr, "Exiting with status %d\n", exit_status);
-    }
-
-  return 0;
+    return 0;
 }
 
-char *words[MAX_WORDS] = {0};
+void manage_background_processes() {
+  pid_t pgid = getpgid(getpid());
 
-/* Splits a string into words delimited by whitespace. Recognizes
- * comments as '#' at the beginning of a word, and backslash escapes.
- *
- * Returns number of words parsed, and updates the words[] array
- * with pointers to the words, each as an allocated string.
- */
-size_t wordsplit(char const *line) {
-  size_t wlen = 0;
-  size_t wind = 0;
-
-  char const *c = line;
-  for (;*c && isspace(*c); ++c); /* discard leading space */
-
-  for (; *c;) {
-    if (wind == MAX_WORDS) break;
-    /* read a word */
-    if (*c == '#') break;
-    for (;*c && !isspace(*c); ++c) {
-      if (*c == '\\') ++c;
-      void *tmp = realloc(words[wind], sizeof **words * (wlen + 2));
-      if (!tmp) err(1, "realloc");
-      words[wind] = tmp;
-      words[wind][wlen++] = *c; 
-      words[wind][wlen] = '\0';
-    }
-    ++wind;
-    wlen = 0;
-    for (;*c && isspace(*c); ++c);
-  }
-  return wind;
-}
-
-
-/* Find next instance of a parameter within a word. Sets
- * start and end pointers to the start and end of the parameter
- * token.
- */
-char
-param_scan(char const *word, char **start, char **end)
-{
-  static char *prev;
-  if (!word) word = prev;
-  
-  char ret = 0;
-  *start = NULL;
-  *end = NULL;
-  char *s = strchr(word, '$');
-  if (s) {
-    char *c = strchr("$!?", s[1]);
-    if (c) {
-      ret = *c;
-      *start = s;
-      *end = s + 2;
-    }
-    else if (s[1] == '{') {
-      char *e = strchr(s + 2, '}');
-      if (e) {
-        ret = '{';
-        *start = s;
-        *end = e + 1;
+  for (pid_t pid = 0; pid < sysconf(_SC_CHILD_MAX); pid++) {
+    if (getpgid(pid) == pgid) {                                                   // Check the status of the process
+      int status;
+      if (waitpid(pid, &status, WNOHANG) == pid) {
+        if (WIFEXITED(status)) {
+          fprintf(stderr, "Child process %jd done. Exit status %d\n", (intmax_t) pid, status);
+        } else if (WIFSIGNALED(status)) {
+          fprintf(stderr, "Child process %jd done. Signaled %d\n", (intmax_t) pid, WTERMSIG(status));
+        }
+      } else if (WIFSTOPPED(status)) {
+        fprintf(stderr, "Child process %jd stopped. Continuing.\n", (intmax_t) pid);
+        kill(pid, SIGCONT);
       }
     }
   }
-  prev = *end;
-  return ret;
-}
-
-/* Simple string-builder function. Builds up a base
- * string by appending supplied strings/character ranges
- * to it.
- */
-char *
-build_str(char const *start, char const *end)
-{
-  static size_t base_len = 0;
-  static char *base = 0;
-
-  if (!start) {
-    /* Reset; new base string, return old one */
-    char *ret = base;
-    base = NULL;
-    base_len = 0;
-    return ret;
-  }
-  /* Append [start, end) to base string 
-   * If end is NULL, append whole start string to base string.
-   * Returns a newly allocated string that the caller must free.
-   */
-  size_t n = end ? end - start : strlen(start);
-  size_t newsize = sizeof *base *(base_len + n + 1);
-  void *tmp = realloc(base, newsize);
-  if (!tmp) err(1, "realloc");
-  base = tmp;
-  memcpy(base + base_len, start, n);
-  base_len += n;
-  base[base_len] = '\0';
-
-  return base;
-}
-
-/* Expands all instances of $! $$ $? and ${param} in a string 
- * Returns a newly allocated string that the caller must free
- */
-char *
-expand(char const *word)
-{
-  char const *pos = word;
-  char *start, *end;
-  char c = param_scan(pos, &start, &end);
-  build_str(NULL, NULL);
-  build_str(pos, start);
-
-  while (c) {
-    if (c == '!') {
-        char bgpid_str[16];                                       // Buffer so background process ID is string
-        snprintf(bgpid_str, sizeof(bgpid_str), "%d", get_last_bg_pid());
-        build_str(bgpid_str, NULL);
-    }
-    else if (c == '$') {
-        char bgpid_str[16];
-        snprintf(pid_str, sizeof(pid_str), "%d", getpid());
-        build_str(pid_str, NULL);                                 // Replace with smallsh process ID
-    }
-    else if (c == '?') {
-        char exit_status_str[16];                                 // Buffer to hold the exit status
-        snprintf(exit_status_str, sizeof(exit_status_str), "%d", get_last_exit_status());
-        build_str(exit_status_str, NULL);
-    }
-    else if (c == '{') {                                        // Extract parameter name from ${parameter}
-        char param_name[end - start - 2];
-        strncpy(param_name, start + 2, end - start - 3);
-        param_name[end - start - 3] = '\0';
-
-        char *param_value = getenv(param_name);                   // Get the environment variable value
-        if (!param_value) {
-            param_value = "";                                       // Default to empty string if variable is unset
-        }
-        build_str(param_value, NULL);
-    } 
-
-    pos = end;
-    c = param_scan(pos, &start, &end);
-    build_str(pos, start);
-  }
-  return build_str(start, NULL);
 }
