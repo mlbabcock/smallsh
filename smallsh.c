4243497ee57c59409bab4bf0c3e344f08c3ab644
mlbabcock <91307276+mlbabcock@users.noreply.github.com>
2023-08-12 16:41:17 -0700

    Update smallsh.c reworked code again

--- smallsh.c
+++ smallsh.c
@@ -13,233 +13,346 @@
 #include <sys/types.h>
 #include <stdint.h>
 
-#ifndef MAX_WORDS
-#define MAX_WORDS 512
-#endif
 
-#define MAX_LINE_LENGTH 1024
-#define MAX_WORD_COUNT 512
+// Global variables
+char *prompt = "$ ";
 
-// Global Functions
-char *words[MAX_WORDS];
-size_t wordsplit(char const *line);
-char * expand(char const *word);
 
-void manage_background_processes();
+// Function prototypes
 void print_prompt();
-char** parse_line(char* line);
-int execute_command(char** words);
+char **parse_line(char *line);
+void expand_parameters(char **args);
+int execute_command(char **args, int *status);
 void handle_signal(int signal);
 
-
-size_t wordsplit(char const *line);
-char param_scan(char const *word, char **start, char **end);
-char *build_str(char const *start, char const *end);
-char *expand(char const *word);
-
-// Global Variables
-char *line;
-char** words;
-int word_count;
-int last_exit_status;
-pid_t last_background_pid;
-
-int is_background_operator(char* word){
-    return strcmp(word, "&") == 0;
-}
-
-int is_redirection_operator(char* word){
-    return strcmp(word, ">") == 0 || strcmp(word, "<") == 0 || strcmp(word, ">>") == 0;
-}
-
-int main(int argc, char *argv[])
-{
-    // Signal Handlers
-    signal(SIGINT, handle_signal);
-    signal(SIGTSTP, handle_signal);
-
-    // Initialize last exit status and background PID
-    last_exit_status = 0;
-    last_background_pid = -1;
-
-    line = malloc(sizeof(char) * MAX_LINE_LENGTH);                      // Buffer to store user's input
-
-    FILE *input = stdin;
-    char *input_fn = "(stdin)";
-
-    if (argc == 2) {
-        input_fn = argv[1];
-        input = fopen(input_fn, "re");
-        if (!input) err(1, "%s", input_fn);
-    } else if (argc > 2) {
-        errx(1, "too many arguments");
-    }
-
-    while (1) {
-    //prompt:;
-        /* TODO: Manage background processes */
-        manage_background_processes();
-
-        /* TODO: prompt */
-        print_prompt();
-
-        // Read line of input
-        int length = MAX_LINE_LENGTH;
-        int line_len = getline(&line, &length, stdin);
-
-        if (line_len == -1 && errno == EINTR) {
-            errno = 0;
-            printf("\n");
-            print_prompt();
-            continue;
-        }
-
-        char* words;
-        words = parse_line(line);
-        expand_words(words);
-        parse_words(words);
-        execute_command(words);
-
-        for (int i = 0; i < word_count; i++) {
-            free(words[i]);
-        }
-        free(words);
-    }
-    return 0;
-}
-
-void print_prompt(){
-    char* user = getenv("USER");
-    char* ps1 = getenv("PS1");
-    fprintf("%s%s$ ", user, ps1);
-}
-
-char** parse_line(char* line) {
-    char* word = strtok(line, " ");
-    char** words = malloc(sizeof(char*) * MAX_WORD_COUNT);
-
-    // iterate over words
-    int word_count = 0;
-    while (word != NULL) {
-        word = expand_word(word);
-
-        words[word_count] = word;
-        word = strtok(NULL, " ");
-        word_count++;
-    }
-    return words;
-}
-
+// Signal handling
 void handle_signal(int signal) {
-    if (signal == SIGINT) {
-        return;
-    }
+  if (signal == SIGINT) {
+    // Ignore this signal
+    return;
+  }
 
-    if (signal == SIGTSTP) {
-        kill(getpid(), SIGSTOP);
-    }
-    errno = 0;
-    printf("\n");
+  // If any other signal is received, print a message and continue
+  fprintf(stderr, "Ignoring signal %d\n", signal);
 }
 
-void parse_words(char** words){
-    for (int i = 0; i < word_count; i++) {
-        if (is_background_operator(words[i])) {
-            words[i] = NULL;
-            words[word_count++] = words[i + 1];
-            i++;
-        } else if (is_redirection_operator(words[i])) {
-            if (words[i + 1] != NULL) {
-                words[i] = words[i] + 1;
-                words[word_count++] = words[i + 1];
-                i++;
-            }
-        }
-    }
-}
+// Main
+int main(int argc, char **argv) {
+  // Set up signal handlers
+  signal(SIGINT, handle_signal);
+  signal(SIGTSTP, SIG_IGN);
 
-int execute_command(char** words) {
-    if (strcmp(words[0], "exit") == 0) {
-        int status;
-        if (words[1] != NULL) {
-            status = atoi(words[1]);
+  // If no arguments are given, then run in interactive mode
+  if (argc == 1) {
+    while (1) {
+      // Print the prompt
+      print_prompt();
+
+      // Get a line of input
+      char *line = NULL;
+      size_t n = 0;
+      ssize_t line_length = getline(&line, &n, stdin);
+      if (line_length == -1) {
+        if (feof(stdin)) {
+          // Exit the shell
+          break;
         } else {
-            status = last_exit_status;
+          // Handle error
+          perror("Failed to read line");
+          continue;
         }
-        exit(status);
-    } else if (strcmp(words[0], "cd") == 0) {
-        if (words[1] == NULL) {
-            chdir("/");
-        } else {
-            chdir(words[1]);
-        }
-    } else {
-        int pid, status;
-        pid_t pid = fork();
-        if (pid == -1) {
-            perror("fork");
-            return -1;
-        } else if (pid == 0) {
-            // Ignore SIGINT and SIGTSTP
-            if (isatty(STDIN_FILENO)) {
-                signal(SIGINT, SIG_IGN);
-                signal(SIGTSTP, SIG_IGN);           
-            }
+      }
 
-            // Handle Redirection
-            for (int i = 0; i < word_count; i++) {
-                if (is_redirection_operator(words[i])) {
-                    char* filename = words[i + 1];
-                    int fd;
-                    if (strcmp(words[i], "<") == 0) {
-                        fd = open(filename, O_RDONLY);
-                    } else if (strcmp(words[i], ">") == 0) {
-                        fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0777);
-                    } else {
-                        fd = open(filename, O_WRONLY | O_APPEND | O_CREAT, 0777);
-                    }
-                    if (fd == -1) {
-                        perror("open");
-                        exit(1);
-                    }
-                    close(0);
-                    dup2(fd, 0);
-                    close(fd);
-                }
-            }
-            int status = execvp(words[0], words);
-            if (status == -1) {
-                perror("execvp");
-                exit(1);
-            }
-        } else {                                                                // Parent Process
-            if (!is_background_operator(words[word_count - 1])) {
-                waitpid(pid, &status, 0);
-                last_exit_status = status;
-            } else {
-                last_background_pid = pid;
-            }
-        }
+      // Parse the line
+      char **args = parse_line(line);
+
+      // Execute the command
+      int status = execute_command(args, status);
+
+      // Free the line of input
+      free(line);
+
+      // Free the arguments
+      for (int i = 0; args[i] != NULL; i++) {
+        free(args[i]);
+      }
+      free(args);
     }
-    return 0;
+  } else if (argc == 2) {
+    // Otherwise, run in non-interactive mode
+    // Execute the specified command
+    int status = execute_command(argv, status);
+
+    // Exit the shell
+    exit(status);
+  } else {
+    // Incorrect usage
+    fprintf(stderr, "Usage: smallsh [script_file]\n");
+    exit(1);
+  }
+
+  return 0;
 }
 
-void manage_background_processes() {
-  pid_t pgid = getpgid(getpid());
+void print_prompt() {
+  printf("%s", prompt);
+}
 
-  for (pid_t pid = 0; pid < sysconf(_SC_CHILD_MAX); pid++) {
-    if (getpgid(pid) == pgid) {                                                   // Check the status of the process
-      int status;
-      if (waitpid(pid, &status, WNOHANG) == pid) {
-        if (WIFEXITED(status)) {
-          fprintf(stderr, "Child process %jd done. Exit status %d\n", (intmax_t) pid, status);
-        } else if (WIFSIGNALED(status)) {
-          fprintf(stderr, "Child process %jd done. Signaled %d\n", (intmax_t) pid, WTERMSIG(status));
+char **parse_line(char *line) {
+  char **args = malloc(sizeof(char *) * 10);
+  int i = 0;
+
+  for (char *token = strtok(line, " "); token != NULL; token = strtok(NULL, " ")) {
+    args[i++] = token;
+  }
+
+  args[i] = NULL;
+  return args;
+}
+
+// Word splitting
+char **parse_line(char *line) {
+  char **args = malloc(sizeof(char *) * 512);
+  int i = 0;
+
+  for (char *token = strtok(line, " "); token != NULL; token = strtok(NULL, " ")) {
+    // Handle backslashes
+    while (strchr(token, '\\') != NULL) {
+      int j = 0;
+      char *new_token = malloc(sizeof(char) * strlen(token) + 1);
+      for (int i = 0; i < strlen(token); i++) {
+        if (token[i] == '\\') {
+          i++;
         }
-      } else if (WIFSTOPPED(status)) {
-        fprintf(stderr, "Child process %jd stopped. Continuing.\n", (intmax_t) pid);
-        kill(pid, SIGCONT);
+        new_token[j++] = token[i];
+      }
+      token = new_token;
+    }
+
+    // Handle comments
+    if (token[0] == '#') {
+      break;
+    }
+    args[i++] = token;
+  }
+  args[i] = NULL;
+  return args;
+}
+
+// Expansion
+void expand_parameters(char **args) {
+  for (int i = 0; args[i] != NULL; i++) {
+    if (args[i][0] == '$') {
+      // Check for special parameters
+      if (args[i][1] == '?') {
+        // $? expands to the exit status of the last foreground command
+        args[i] = itoa(get_exit_status());
+      } else if (args[i][1] == '!') {
+        // $! expands to the pid of the most recent background process
+        args[i] = itoa(get_pid_of_last_background_process());
+      } else if (args[i][1] == '$') {
+        // $$ expands to the pid of the current shell process
+        args[i] = itoa(getpid());
+      } else {
+        // Otherwise, expand the parameter as a variable
+        args[i] = getenv(args[i] + 2);
+        if (args[i] == NULL) {
+          args[i] = "";
+        }
       }
     }
   }
+}
+
+// Execution
+int execute_command(char **args, int *status) {
+  int i = 0;
+
+  // Fork a child process
+  pid_t pid = fork();
+  if (pid == -1) {
+    // Error forking
+    fprintf(stderr, "Failed to fork\n");
+    return 1;
+  }
+
+  if (pid == 0) {
+    // Child process
+    // Reset all signals to their original dispositions
+    sigset_t set;
+    sigfillset(&set);
+    sigprocmask(SIG_SETMASK, &set, NULL);
+
+    // Handle redirection
+    for (i = 0; args[i] != NULL; i++) {
+      if (args[i][0] == '<') {
+        // Input redirection
+        int fd = open(args[i + 1], O_RDONLY);
+        if (fd == -1) {
+          perror("Failed to open input file");
+          exit(1);
+        }
+
+        // Redirect stdin
+        dup2(fd, 0);
+        close(fd);
+
+        // Remove the redirection operator from the arguments
+        args[i] = args[i + 1];
+        args[i + 1] = NULL;
+      } else if (args[i][0] == '>') {
+        // Output redirection
+        int fd = open(args[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+        if (fd == -1) {
+          perror("Failed to open output file");
+          exit(1);
+        }
+
+        // Redirect stdout
+        dup2(fd, 1);
+        close(fd);
+
+        // Remove the redirection operator from the arguments
+        args[i] = args[i + 1];
+        args[i + 1] = NULL;
+      }
+    }
+
+    // Execute the command
+    if (execvp(args[0], args) == -1) {
+      // Command not found
+      perror("Failed to execute command");
+      exit(1);
+    }
+  } else {
+    // Parent process
+    // If the command is not in the background, wait for it to finish
+    if (args[i] != NULL && args[i][0] != '&') {
+      waitpid(pid, status, 0);
+      set_exit_status(*status);
+    } else {
+      // The command is in the background
+      // Set the $! variable
+      set_pid_of_last_background_process(pid);
+    }
+  }
+
+  return *status;
+}
+
+
+// Waiting
+void handle_signal(int signal) {
+  if (signal == SIGINT || signal == SIGTSTP) {
+    // Ignore these signals
+    return;
+  }
+
+  // If any other signal is received, print a message and continue
+  fprintf(stderr, "Ignoring signal %d\n", signal);
+}
+
+// Parsing
+int parse_line(char *line) {
+  char **args = malloc(sizeof(char *) * 512);
+  int i = 0;
+
+  // Split the line into words
+  for (char *token = strtok(line, " "); token != NULL; token = strtok(NULL, " ")) {
+    // Handle backslashes
+    while (strchr(token, '\\') != NULL) {
+      int j = 0;
+      char *new_token = malloc(sizeof(char) * strlen(token) + 1);
+      for (int i = 0; i < strlen(token); i++) {
+        if (token[i] == '\\') {
+          i++;
+        }
+        new_token[j++] = token[i];
+      }
+      token = new_token;
+    }
+    // Handle comments
+    if (token[0] == '#') {
+      break;
+    }
+    args[i++] = token;
+  }
+  args[i] = NULL;
+  return i;
+}
+
+// Input and Output redirection
+int execute_command(char **args, int *status) {
+  int i = 0;
+
+  // Fork a child process
+  pid_t pid = fork();
+  if (pid == -1) {
+    // Error forking
+    fprintf(stderr, "Failed to fork\n");
+    return 1;
+  }
+
+  if (pid == 0) {
+    // Child process
+    // Reset all signals to their original dispositions
+    sigset_t set;
+    sigfillset(&set);
+    sigprocmask(SIG_SETMASK, &set, NULL);
+
+    // Handle redirection
+    for (i = 0; args[i] != NULL; i++) {
+      if (args[i][0] == '<') {
+        // Input redirection
+        int fd = open(args[i + 1], O_RDONLY);
+        if (fd == -1) {
+          perror("Failed to open input file");
+          exit(1);
+        }
+
+        // Redirect stdin
+        dup2(fd, 0);
+        close(fd);
+
+        // Remove the redirection operator from the arguments
+        args[i] = args[i + 1];
+        args[i + 1] = NULL;
+      } else if (args[i][0] == '>') {
+        // Output redirection
+        int fd = open(args[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+        if (fd == -1) {
+          perror("Failed to open output file");
+          exit(1);
+        }
+
+        // Redirect stdout
+        dup2(fd, 1);
+        close(fd);
+
+        // Remove the redirection operator from the arguments
+        args[i] = args[i + 1];
+        args[i + 1] = NULL;
+      }
+    }
+
+    // Execute the command
+    if (execvp(args[0], args) == -1) {
+      // Command not found
+      perror("Failed to execute command");
+      exit(1);
+    }
+  } else {
+    // Parent process
+    // If the command is not in the background, wait for it to finish
+    if (args[i] != NULL && args[i][0] != '&') {
+      waitpid(pid, status, 0);
+      set_exit_status(*status);
+    } else {
+      // The command is in the background
+      // Set the $! variable
+      set_pid_of_last_background_process(pid);
+    }
+  }
+
+  return *status;
 }
